ae40105e6188d7aa5b4611da9da8a195fea55d79
Alvin De Castro <alvin.dcastro@gmail.com>
2019-07-07 17:35:06 +0800

    Updates
    Understood interfaces
    Added table test
    Added Triangle struct
    Added Triangle shapes

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,42 @@
 package structs
 
-//Rectangle - struct
+import "math"
+
+//Shape - Interface
+type Shape interface {
+	Area() float64
+}
+
+//Rectangle - Struct
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+//Area of Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+//Circle - Struct
+type Circle struct {
+	Radius float64
+}
+
+//Area of Circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+//Triangle - Struct
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+//Area of Triangle
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 //Perimeter - Initial code

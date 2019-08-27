da41a4b46f3f609e78e6aee92fe8be574f8a2957
Alvin De Castro <alvin.decastro@ncs.com.sg>
2019-08-27 18:08:59 +0800

    Updates

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,28 @@
 package maps
 
+import (
+	"errors"
+)
+
 //Dictionary wrapper
 type Dictionary map[string]string
 
+//ErrNotFound var
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+// Add function
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
 // Search using Dictionary
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
 
 // Search - initial function
